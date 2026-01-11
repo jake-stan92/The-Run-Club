bf4bcfd902f6f19a9c4c643240045dade86efadf
Jake Stanier <jakestanier@Jakes-MacBook-Pro.local>
2026-01-11 16:30:49 -0500

    club stat page css added

--- front-end/src/components/ClubStatTable.jsx
+++ front-end/src/components/ClubStatTable.jsx
@@ -1,3 +1,7 @@
+import goldTrophy from "../assets/images/gold-trophy.svg";
+import silverTrophy from "../assets/images/silver-trophy.svg";
+import bronzeTrophy from "../assets/images/bronze-trophy.svg";
+
 import "./ClubStatTable.css";
 
 export default function ClubStatTable({ tableData, filter }) {
@@ -33,9 +37,31 @@ export default function ClubStatTable({ tableData, filter }) {
         </tr>
       </thead>
       <tbody>
-        {sortedTableData.map((athlete) => (
+        {sortedTableData.map((athlete, index) => (
           <tr key={athlete.name}>
-            <td className="club-stats-athlete-name-column">{athlete.name}</td>
+            {/* <td className="club-stats-athlete-name-column">
+              {index === 0 ? (
+                <img src={goldTrophy} />
+              ) : index === 1 ? (
+                <img src={silverTrophy} />
+              ) : index === 2 ? (
+                <img src={bronzeTrophy} />
+              ) : null}
+              <p>{athlete.name}</p>
+            </td> */}
+            <td className="club-stats-athlete-name-column">
+              {index < 3 ? (
+                <img
+                  src={[goldTrophy, silverTrophy, bronzeTrophy][index]}
+                  alt={`Place ${index + 1}`}
+                />
+              ) : (
+                <p className="clubs-stats-athlete-name-column-img-placeholder">
+                  {index + 1}.
+                </p>
+              )}
+              <p>{athlete.name}</p>
+            </td>
             {/* <td>{athlete.numWalks}</td> */}
             {filter === "Walk" && <td>{athlete.walkTotalDistance}</td>}
 
